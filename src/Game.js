6291d16ac2201ff98de152410438594de746ddf5
Aidin Rezaei <82445380+aidin-rezaei@users.noreply.github.com>
2022-04-01 05:19:50 +0430

    update

--- src/Game.js
+++ src/Game.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 // import { Link } from "react-router-dom";
 import Cookies from 'js-cookie'
 function Game() {
@@ -11,8 +11,36 @@ function Game() {
     const [classmodlink, setclassmodlink] = useState("succopy")
     const [classopenmodpl, setclassopenmodpl] = useState("boxpleyer")
     const [valuejavab, setvaluejavab] = useState("")
+    const [onlinepleyerstate, setonlinepleyerstate] = useState([])
+    const [classgetkalame, setclassgetkalame] = useState('')
+    const [kalameste, setkalameste] = useState([])
+    // const [dataroom, setdataroom] = useState([])
+    // const [javabjson, setjavabjson] = useState()
+    const [javaborg, setjavaborg] = useState([])
+    const [soaljson, setsoaljson] = useState([])
+    const onlinepleyer = () => {
+        fetch("https://aidinr.ir/multichar/api.php?hashroomp=" + Cookies.get('room') + "&hashpleyer=" + Cookies.get('name'))
+            .then(response => response.json())
+            .then(data => {
+                setonlinepleyerstate(data.online)
+                // setdataroom(data.room[0])
+                // setjavabjson(data.room.javabjson[0])
+                setjavaborg(data.room.javaborg[0])
+                setsoaljson(data.room.soaljson[0])
+                if (Cookies.get('active') === 'yes') {
+                    setclassgetkalame('getkalame')
+                }
+                if (Cookies.get('active') === 'no') {
+                    setclassgetkalame('getkalame offfkalame')
+                }
+                console.clear()
+            })
+            .finally(() => setTimeout(onlinepleyer, 1000))
+    }
+    useEffect(onlinepleyer, [])
+    // setInterval(onlinepleyer, 1000)
     const copylinkgame = () => {
-        navigator.clipboard.writeText("https://aidinr.ir/multichar?token=q213q323");
+        navigator.clipboard.writeText(Cookies.get('room'));
         setclassmodlink("succopy calssanimcopy")
         setTimeout(() => {
             setclassmodlink("succopy ")
@@ -25,32 +53,94 @@ function Game() {
             setclassopenmodpl("boxpleyer")
         }
     }
-
     const clickkalame = (e) => {
         e.target.className = "disblep"
         setvaluejavab(valuejavab + e.target.innerText)
+        let sdfdsf = valuejavab + e.target.innerText;
+        setTimeout(() => {
+            if (sdfdsf.length === soaljson.length) {
+                setvaluejavab('')
+                let element = document.querySelectorAll(".disblep");
+                window.navigator.vibrate(300);
+                if (sdfdsf === javaborg) {
+                    Cookies.set('active', "yes")
+                    setclassgetkalame('getkalame')
+                }
+                // window.navigator.vibrate([300, 100, 200, 50, 300]);
+                for (let i = 0; element.length >= i; i++) {
+                    element[i].classList.remove("disblep");
+                }
+            }
+        }, 500)
+
+    }
+    // useEffect(clickkalame)
+    const getkalamein = (e) => {
+        setkalameste(e.target.value)
+
+
     }
 
+    const subkalame = () => {
+        if (kalameste !== '') {
+            httpGet("https://aidinr.ir/multichar/api.php?roomkalame=" + Cookies.get('room') + "&kalames=" + kalameste)
+            Cookies.set('active', "no")
+            setclassgetkalame('getkalame offfkalame')
+        }
 
+    }
+
+    const pleyersgame2 = onlinepleyerstate.map(pleyer => {
+
+        return (<div className="itempleyer">
+            <i className="fa-solid fa-gamepad"></i><span>{pleyer.name}</span><p>{(Cookies.get('name') === pleyer.hash) ? 'you' : ''}</p>
+        </div>)
+    })
+    const soaljson2 = soaljson.map((soal) => {
+
+        return (<p onClick={clickkalame}>{soal}</p>)
+    })
+    window.addEventListener("load", async function () {
+        httpGet("https://aidinr.ir/multichar/api.php?stuse=online&shash=" + Cookies.get('name'))
+        if (Cookies.get('name') === undefined) {
+            window.location.href = '/'
+        }
+    })
     window.addEventListener("beforeunload", function () {
         httpGet("https://aidinr.ir/multichar/api.php?stuse=offline&shash=" + Cookies.get('name'))
-    })
-    window.addEventListener("load", function () {
-        httpGet("https://aidinr.ir/multichar/api.php?stuse=online&shash=" + Cookies.get('name'))
+        if (onlinepleyerstate.length <= 0) {
+            httpGet("https://aidinr.ir/multichar/api.php?dellroome=" + Cookies.get('room'))
+        }
+        Cookies.remove('name')
+        Cookies.remove('active')
+        Cookies.remove('room')
     })
 
     return (
         <div className="fullhome background" >
+            <div className={classgetkalame}>
+                <div className="titlelogo">
+
+                    <h1 >MULTI CHAR</h1>
+                    <a href="https://aidinr.ir" className="aidinr">power by Aidinr.ir</a>
+                </div>
+                <div className="box-join">
+                    <h2 className="titleboxhome">enter a word</h2>
+                    <input className="inputtokn" type="text" onChange={getkalamein} placeholder="Write the word" />
+                    <button className="btn" onClick={subkalame}>submit</button>
+                </div>
+            </div>
             <div className={classopenmodpl} >
-                <div className="btnopenbox" onClick={openmodpl}>
+                <div className="btnopenbox" onClick={() => {
+                    openmodpl()
+                    onlinepleyer()
+                }}>
                     <i className="fa fa-angle-up"></i>
                     <p>pleyers</p>
                     <i className="fa fa-angle-down closeplred"></i>
                 </div>
                 <div className="cantentpleyer">
-                    <div className="itempleyer"><i class="fa-solid fa-gamepad"></i><span>aidin</span></div>
-                    <div className="itempleyer"><i class="fa-solid fa-gamepad"></i><span>amirhossin</span></div>
-                    <div className="itempleyer"><i class="fa-solid fa-gamepad"></i><span>mohammad</span></div>
+                    {pleyersgame2}
                 </div>
             </div>
             <div className="title-game">
@@ -65,24 +155,16 @@ function Game() {
                 <div className="exit" ><i className="fa fa-arrow-right-from-bracket"></i> <span>Exit</span></div>
             </div>
             <div className="copylink" onClick={copylinkgame}>
-                <p className="linkgame">https://aidinr.ir/multichar?token=q213q323</p>
-                <button>copy link</button>
+                <p className="linkgame">{Cookies.get('room')}</p>
+                <button>copy token</button>
                 <p className={classmodlink}>Copy successful</p>
             </div>
-
 
             <div className="boxjvab">
                 <p>{valuejavab}</p>
             </div>
             <div className="horofbox">
-                <p onClick={clickkalame}>گ</p>
-                <p onClick={clickkalame}>و</p>
-                <p onClick={clickkalame}>ج</p>
-                <p onClick={clickkalame}>ه</p>
-                <p onClick={clickkalame}>گ</p>
-                <p onClick={clickkalame}>و</p>
-                <p onClick={clickkalame}>ج</p>
-                <p onClick={clickkalame}>ه</p>
+                {soaljson2}
             </div>
 
 
--- src/Home.js
+++ src/Home.js
@@ -18,21 +18,52 @@ function Home() {
         xmlHttp.send(null);
         return xmlHttp.responseText;
     }
+
     const [user, setUser] = useState(Cookies.get('name'))
     const [classmodal, setclassmodal] = useState("modaluser")
     const [valname, setvalname] = useState("")
+    const [hashroom, sethashroom] = useState("")
+    const [getidroom, setgetidroom] = useState("")
     const valuename = (e) => {
         setvalname(e.target.value)
         setUser(makeid(8))
+        sethashroom(makeid(8))
     }
 
     const login = (e) => {
         if (valname !== '') {
             Cookies.set('name', user)
             setclassmodal("modaluser hidemodal")
-            httpGet("https://aidinr.ir/multichar/api.php?name="+valname+"&hash="+user)
+            httpGet("https://aidinr.ir/multichar/api.php?name=" + valname + "&hash=" + user)
         }
     }
+    const loginenter = (e) => {
+        if (e.key === 'Enter') {
+            if (valname !== '') {
+                Cookies.set('name', user)
+                setclassmodal("modaluser hidemodal")
+                httpGet("https://aidinr.ir/multichar/api.php?name=" + valname + "&hash=" + user)
+            }
+        }
+    }
+
+    const createroom = () => {
+        httpGet("https://aidinr.ir/multichar/api.php?hashroom=" + hashroom + "&active=" + user)
+        httpGet("https://aidinr.ir/multichar/api.php?roomid=" + hashroom + "&porsenid=" + user)
+        Cookies.set('room', hashroom)
+        Cookies.set('active', "yes")
+    }
+    const gettokenroom = (e) => {
+        setgetidroom(e.target.value)
+    }
+    const jointhegame = () => {
+        if (getidroom !== '') {
+            httpGet("https://aidinr.ir/multichar/api.php?roomid=" + getidroom + "&porsenid=" + user)
+            Cookies.set('room', getidroom)
+            Cookies.set('active', "no")
+        }
+    }
+
     return (
         <div className="fullhome">
             <div className={classmodal}>
@@ -43,7 +74,7 @@ function Home() {
                 </div>
                 <div className="box-join">
                     <h2 className="titleboxhome">enter your name</h2>
-                    <input className="inputtokn" type="text" onChange={valuename} placeholder="Write the your name" />
+                    <input className="inputtokn" type="text" onChange={valuename} onKeyDown={loginenter} placeholder="Write the your name" />
                     <button onClick={login} className="btn">login</button>
                 </div>
             </div>
@@ -55,12 +86,12 @@ function Home() {
             <div className="box-create">
                 <h3 className="titleboxhome wellcome">wellcome {valname}</h3>
                 <h2 className="titleboxhome">create game</h2>
-                <Link to="/game" className="btn">create</Link>
+                <Link to={"/game?room=" + hashroom} className="btn" onClick={createroom}>create</Link>
             </div>
             <div className="box-join">
                 <h2 className="titleboxhome">join the game</h2>
-                <input className="inputtokn" type="text" placeholder="Write the token" />
-                <Link to="/game" className="btn">join</Link>
+                <input className="inputtokn" type="text" onChange={gettokenroom} placeholder="Write the token" />
+                <Link to="/game" className="btn" onClick={jointhegame}>join</Link>
             </div>
         </div>
     );
